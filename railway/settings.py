72c3d3407c0ff2cf607132a017ce0ccbfe702653
Achal Pathak <pathak.achal5@gmail.com>
2019-09-13 16:25:30 +0530

    changed requirements.txt

--- railway/settings.py
+++ railway/settings.py
@@ -131,11 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.Users'
 
@@ -152,4 +147,3 @@ EMAIL_HOST_USER = 'tester.ak1234@gmail.com'
 EMAIL_HOST_PASSWORD = 'test.123456'
 EMAIL_PORT = 587
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
