93402a8653d5252a6bad3e7632e56cffae7772c3
Achal Pathak <pathak.achal5@gmail.com>
2019-10-21 21:19:47 +0530

    first commit

--- railway/settings.py
+++ railway/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=*&4p@o=y_c6nv#5*c&5y+$-!hm+ou94@fws*h9h^yt3a2waw$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.10']
 
 
 # Application definition
